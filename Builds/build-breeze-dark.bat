001dd06c91b73ff3eaefe48d51660b3b7ce1a32f
fast-geek <rodiongudz.services@gmail.com>
2020-11-29 17:35:19 +0300

    Project update

--- Builds/build-breeze-dark.bat
+++
@@ -1,3 +0,0 @@
-set SCRIPT_DIR=BreezeStyleSheets
-set ICONS_DIR=icons/nowshed
-make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -output breeze-dark  -config dark-config.json -icons-dir %ICONS_DIR% -style dark.qss 
